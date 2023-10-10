d097471729f911777923e2ff0aa39510ba5cd39c
Alex Costa <89145796+AlexBCosta@users.noreply.github.com>
2023-10-10 15:41:49 -0300

    Protegendo Aplicação contra Serialização

---
+++ src/main/java/com/alex/helpdesk/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.alex.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alex.helpdesk.domain.Tecnico;
+import com.alex.helpdesk.repositores.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository repository;
+
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
